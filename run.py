74a13b849f50d0466672e0c5379ffe3b2bce0794
Chris France <christopherbfrance@gmail.com>
2023-07-22 23:24:12 -0700

    Faster, 10 tracks

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import mido
 from mido import Message, MidiFile, MidiTrack
 
 
-def create_midi_file(filename, repeats):
+def create_midi_file(filename, repeats=10):
     # Define a dictionary to map instruments to MIDI notes
     note_values = {
         "B": 36,  # Conga Bass
@@ -12,15 +12,17 @@ def create_midi_file(filename, repeats):
         "S": 38,  # Conga Slap
         "o": 39,  # Bongo Open
         "s": 40,  # Bongo Slap
-        "H": 41,  # High Conga
-        "L": 42,  # Low Conga
-        "c": 43,  # Clave
+        "H": 41,  # Closed Hi-hat
+        "L": 42,  # Low Tom
+        "c": 43,  # Clap
         "m": 44,  # Maracas
-        "X": 45,  # Guiro
-        "N": 46,  # Timbale High
-        "C": 47,  # Cowbell High
-        "T": 48,  # Timbale Low
-        "t": 49,  # Cowbell Low
+        "X": 45,  # High Tom
+        "M": 46,  # Open Hi-hat
+        "N": 47,  # Low-Mid Tom
+        "C": 48,  # High-Mid Tom
+        "T": 49,  # Crash Cymbal 1
+        "t": 50,  # High Tom 2
+        # Add more instruments as needed...
     }
 
     # Define the drum pattern
@@ -37,26 +39,31 @@ def create_midi_file(filename, repeats):
     --------t---------------t---
     """
 
-    # Create a new MIDI file with a single track
+    # Create a new MIDI file
     mid = MidiFile()
-    track = MidiTrack()
-    mid.tracks.append(track)
 
-    # Repeat the drum pattern
-    for _ in range(repeats):
-        # Process each line of the drum pattern
-        for line in drum_pattern.strip().split("\n"):
-            # Process each character in the line
+    # Process each line of the drum pattern
+    for line in drum_pattern.strip().split("\n"):
+        track = MidiTrack()
+        mid.tracks.append(track)
+
+        for _ in range(repeats):
+            tick = 0  # Reset tick for each repeat
             for char in line:
                 # If the character is a note, add it to the track
                 if char in note_values:
                     note = note_values[char]
                     track.append(
-                        Message("note_on", note=note, velocity=64, time=0, channel=9)
+                        Message("note_on", note=note, velocity=64, time=tick, channel=9)
                     )
-                    track.append(
-                        Message("note_off", note=note, velocity=64, time=480, channel=9)
-                    )  # 480 ticks is a quarter note in MIDI, you may need to adjust this
+                    tick += 10  # Advance the tick by one note length
+                else:
+                    tick += 10  # Advance the tick for a rest beat
+
+                # Silence after each note
+                track.append(
+                    Message("note_off", note=note, velocity=64, time=tick, channel=9)
+                )
 
     # Save the MIDI file
     mid.save(filename)
